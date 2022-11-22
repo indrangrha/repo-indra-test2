54c5d8fd19bcfac3066e3d3ddec5dadd88f86e08
Indra Nugraha <118705976+indrangrha@users.noreply.github.com>
2022-11-22 15:44:33 +0700

    mengubah isi index.html tag h

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <title>Coba git remote</title>
 </head>
 <body>
-    <h1>Hello World</h1>
-    <h2>baru nuuh</h2>
+    <h1>Hello Dunia!</h1>
+    <h2>baru nihh</h2>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci quisquam laudantium velit at atque nostrum illo quae. Harum magnam, debitis nesciunt quasi a, impedit tenetur molestiae possimus amet odio illum.</p>
 </body>
 </html>
